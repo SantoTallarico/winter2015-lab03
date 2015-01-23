17726a7634a6ad023d9d1c38f48bc5ac169b304a
Santo Tallarico <>
2015-01-23 11:26:31 -0800

    Creating necessary controllers Signed-off-by:Santo Tallarico

---
+++ application/controllers/Guess.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Guess
+ *
+ * @author ShirleyPC
+ */
+class Guess extends Application {
+    //put your code here
+}
---
+++ application/controllers/last/Last.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Last
+ *
+ * @author ShirleyPC
+ */
+class Last extends Application {
+    //put your code here
+}
